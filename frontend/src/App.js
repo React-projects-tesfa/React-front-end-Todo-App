4620d18bcbc6e9dc3fe88ffa3ba3d36a10944742
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-11 17:58:14 -0500

    added strikethrought and delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,9 @@ class App extends React.Component {
       this.handleChange = this.handleChange.bind(this)
       this.handleSubmit = this.handleSubmit.bind(this)
       this.getCookie = this.getCookie.bind(this)
+      this.startEdit = this.startEdit.bind(this)
+      this.deleteItem = this.deleteItem.bind(this)
+      this.strikeUnstrike = this.strikeUnstrike.bind(this)
   };
 
   getCookie(name) {
@@ -78,6 +81,16 @@ class App extends React.Component {
     var csrftocken = this.getCookie('csrftocken')
 
     var url = 'http://127.0.0.1:8000/api/task-create/'
+
+    if(this.state.editing == true){
+      url = `http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}`
+
+      this.setState({
+        editing: false
+      })
+    }
+    //console.log(this.state.activeItem)
+
     fetch(url, {
       method: 'POST',
       headers:{
@@ -106,6 +119,39 @@ class App extends React.Component {
     })
   }
 
+  strikeUnstrike(task){
+    task.completed = !task.completed
+    console.log("Task: ", task.completed)
+
+    var csrftocken = this.getCookie('csrftocken')
+    var url=`http://127.0.0.1:8000/api/task-update/${task.id}`
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrftocken,
+      },
+      body: JSON.stringify({'completed': task.completed, 'title' : task.title})
+    }).then(() => {
+      this.fetchTasks()
+    })
+  }
+
+  deleteItem(task){
+    var csrftocken = this.getCookie('csrftocken')
+
+    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}`, {
+      method: 'DELETE',
+      headers : {
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrftocken,
+      }
+    }).then((response) => {
+      this.fetchTasks()
+    })
+  }
+
 
   render(){
     var tasks = this.state.todoList
@@ -128,13 +174,18 @@ class App extends React.Component {
             { tasks.map(function(task, index){
               return(
                 <div key={index} className="task-wrapper flex-wrapper">
-                  <div style={{flex:7}}>
-                    <span>{task.title}</span>
+                  <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
+                    {task.completed == false ? (
+                      <span>{task.title}</span>
+                      ) : (
+                      <strike>{task.title}</strike>
+                      )}
+                    
                   </div>
                   <div style={{flex:1}}>
                     <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info"> Edit</button>
                   </div>
-                  <div style={{flex:1}}>
+                  <div onClick={() => self.deleteItem(task)} style={{flex:1}}>
                     <button className="btn btn-sm btn-outline-dark delete">-</button>
                   </div>
                 </div>
